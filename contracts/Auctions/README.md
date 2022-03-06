2057ac91d3d53db9fe62c3e7ef15afffb12dada9
Avraam Marimpis <makhsm@gmail.com>
2022-03-06 12:48:13 +0100

    Add blind auction contract.

--- contracts/Auctions/README.md
+++ contracts/Auctions/README.md
@@ -1,6 +1,8 @@
 
 Contracts are from:
 
-* https://solidity-by-example.org/app/dutch-auction/
+* [Dutch Auction](https://solidity-by-example.org/app/dutch-auction/)
 
 * [English Auction]https://solidity-by-example.org/app/english-auction/
+
+* [Blind Auction](https://www.youtube.com/watch?v=TGJ1WLLw9ic)
---
+++ contracts/Auctions/blind_auction.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract BlindAuction {
+
+    // VARIABLES
+    struct Bid {
+        bytes32 blindedBid;
+        uint deposit;
+    }
+
+    address payable public beneficiary;
+    uint public biddingEnd;
+    uint public revealEnd;
+    bool public ended;
+
+    mapping(address => Bid[]) public bids;
+
+    address public highestBidder;
+    uint public highestBid;
+
+    mapping(address => uint) pendingReturns;
+
+    // EVENTS
+    event AuctionEnded(address winner, uint highestBid);
+
+    // MODIFIERS
+    modifier onlyBefore(uint _time) { require(block.timestamp < _time); _; }
+    modifier onlyAfter(uint _time) { require(block.timestamp > _time); _; }
+
+    // FUNCTIONS
+    constructor(
+        uint _biddingTime,
+        uint _revealTime,
+        address payable _beneficiary
+    ) {
+        beneficiary = _beneficiary;
+        biddingEnd = block.timestamp + _biddingTime;
+
+    }
+
+    function generateBlindedBidBytes32(uint value, bool fake) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(value, fake));
+    }
+
+    function bid(bytes32 _blindedBid) public payable onlyBefore(biddingEnd) {
+        bids[msg.sender].push(Bid({
+            blindedBid: _blindedBid;
+            deposit: msg.value;
+        }))
+    }
+
+    function reveal(
+        uint[] memory _values,
+        bool[] memory _fake
+    )   public 
+        onlyAfter(biddingEnd)
+        onlyBefore(revealEnd) 
+    {
+        uint length = bids[msg.sender].length;
+        require(_values.length == length);
+        require(_fake.length == length);
+
+        uint refund;
+        for (uint i=0; i<length; i++) {
+            Bid storage bidToCheck = bids[msg.sender][i];
+            (uint value, bool fake) = (_values[i], _fake[i]);
+
+            if (bidToCheck.blindedBid != keccak256(abi.encodePacked(value, fake))) {
+                continue;
+            }
+
+            refund +=  bidToCheck.deposit;
+            if (!fake && bidToCheck.deposit >= value) {
+                if (!placeBid(msg.sender, value)) {
+                    // refund -= value;
+                    payable(msg.sender).transfer(bidToCheck.deposit * (1 ether));
+                }
+            }
+            bidToCheck.blindedBid = bytes32(0);
+        }
+        payable(msg.sender).transfer(refund);
+    }
+
+    function auctionEnd() public payable onlyAfter(revealEnd) {
+        require(!ended);
+        emit AuctionEnded(highestBidder, highestBidder);
+        ended = true;
+        beneficiary.transfer(highestBid);
+    }
+
+    function withdraw() public {
+        uint amount = pendingReturn[msg.sender];
+        if (amount >0) {
+            pendingReturns[msg.sender] = 0;
+            payable(msg.sender).transfer(amount * (1 ether));
+        }
+    }
+
+    function placeBid(address bidder, uint value) internal returns(bool success) {
+        if (value <= highestBid) {
+            return false;
+        }
+
+        if (highestBidder != address(0)) {
+            pendingReturns[highestBidder] += highestBid;
+        }
+
+        highestBid = value;
+        highestBidder = bidder;
+        
+        return true;
+    }
+
+
+}
